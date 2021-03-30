940ddf3d2accb8d11cbb8409fb9178416fd6e99b
Bogdyapple14 <bogdybogdan34@gmail.com>
2021-03-30 21:44:26 +0300

    Breeds pagination done

--- src/Compoents/All Breeds/Breeds.js
+++ src/Compoents/All Breeds/Breeds.js
@@ -3,24 +3,34 @@ import "../All Breeds/Breeds.scss";
 import axios from "axios";
 import Breed from "../Breed/Breed";
 import BreedPopup from "../BreedPopup/BreedPopup";
+import { Pagination } from "@material-ui/lab";
 
 export default function Breeds() {
   const [breeds, setBreeds] = useState([]);
   const [loading, setLoading] = useState(false);
   const [displayPopup, setDisplayPopup] = useState(false);
   const [clickedCat, setClickedCat] = useState();
+  const [pagesNumber, setPagesNumber] = useState(0);
+  const [currentPage, setCurrentPage] = useState(0);
 
   useEffect(() => {
-    fetchBreeds();
-  }, []);
+    fetchBreeds(currentPage, 6);
+    document.addEventListener("keydown", escFunction, false);
+    return () => {
+      document.removeEventListener("keydown", escFunction, false);
+    };
+  }, [currentPage]);
 
-  const fetchBreeds = async () => {
+  const fetchBreeds = async (page, limit) => {
     setLoading(true);
     await axios
-      .get("https://api.thecatapi.com/v1/breeds")
+      .get(`https://api.thecatapi.com/v1/breeds?limit=${limit}&page=${page}`)
       .then((res) => {
-        const data = res.data;
-        setBreeds(data);
+        const pages = Math.ceil(
+          +res.headers["pagination-count"] / +res.headers["pagination-limit"]
+        );
+        setPagesNumber(pages);
+        setBreeds(res.data);
         setLoading(false);
       })
       .catch((err) => {
@@ -32,44 +42,49 @@ export default function Breeds() {
     if (e.keyCode === 27) setDisplayPopup(false);
   };
 
-  useEffect(() => {
-    document.addEventListener("keydown", escFunction, false);
-
-    return () => {
-      document.removeEventListener("keydown", escFunction, false);
-    };
-  }, []);
-
   const togglePopup = () => {
     setDisplayPopup(!displayPopup);
+  };
+
+  const changePage = (e, value) => {
+    if (currentPage !== value - 1) setCurrentPage(value - 1);
   };
 
   return (
     <div className="breeds-container">
       {!loading ? (
-        breeds.map((breed) =>
-          breed.image ? (
-            <div
-              className="breed"
-              onKeyPress={escFunction}
-              onClick={togglePopup}
-              key={breed.id}
-            >
-              <Breed
-                clickedCat={clickedCat}
-                setClickedCat={setClickedCat}
-                breeds={breeds}
-                id={breed.id}
-                url={breed.image.url}
-                catName={breed.name}
-                origin={breed.origin}
-                cfa={breed.cfa_url}
-                description={breed.description}
-                intelligence={breed.intelligence}
-              />
-            </div>
-          ) : null
-        )
+        <div className="breeds-wrap">
+          {breeds.map((breed) =>
+            breed.image ? (
+              <div
+                className="breed"
+                onKeyPress={escFunction}
+                onClick={togglePopup}
+                key={breed.id}
+              >
+                <Breed
+                  clickedCat={clickedCat}
+                  setClickedCat={setClickedCat}
+                  breeds={breeds}
+                  id={breed.id}
+                  url={breed.image.url}
+                  catName={breed.name}
+                  origin={breed.origin}
+                  cfa={breed.cfa_url}
+                  description={breed.description}
+                  intelligence={breed.intelligence}
+                />
+              </div>
+            ) : null
+          )}
+          <div className="pagination">
+            <Pagination
+              count={pagesNumber}
+              onChange={changePage}
+              page={currentPage + 1}
+            />
+          </div>
+        </div>
       ) : (
         <h2>Loading...</h2>
       )}
